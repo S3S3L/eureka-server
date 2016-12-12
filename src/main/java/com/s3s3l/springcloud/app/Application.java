218629195c99d5abe8de6c22746301bd1e76dbb1
kehewei <kehewei@cheyaoshi.com>
2016-12-12 18:59:01 +0800

    更好的写法

--- src/main/java/com/s3s3l/springcloud/app/Application.java
+++ src/main/java/com/s3s3l/springcloud/app/Application.java
@@ -9,8 +9,8 @@
 
 package com.s3s3l.springcloud.app;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
@@ -28,6 +28,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class Application {
 
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(Application.class).web(true).run(args);
+		SpringApplication.run(Application.class, args);
 	}
 }
